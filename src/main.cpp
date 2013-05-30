a6d8e83d6d4f9cef3fe9d34244780fae10b71d81
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-30 22:43:16 +0200

    Added main.cpp and a draft of a working build system. MainWindow seems to be working.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <QApplication>
+#include <QTextCodec>
+#include "MainWindow.h"
+
+int main(int argc, char** argv) {	
+	QApplication application(argc, argv);
+	
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+	QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+	
+	MainWindow mainWindow;
+	
+	mainWindow.show();
+	return application.exec();
+}
