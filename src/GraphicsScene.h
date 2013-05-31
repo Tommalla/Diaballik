e8a8e7416ca9fa66166ad011cb5c7855319d31bc
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-31 23:46:15 +0200

    Added a draft of GraphicsScene.

---
+++ src/GraphicsScene.h
@@ -0,0 +1,17 @@
+/* Tomasz [Tommalla] Zakrzewski, 2013
+All rights reserved */
+
+#ifndef GRAPHICSSCENE_H
+#define GRAPHICSSCENE_H
+
+#include <QGraphicsScene>
+
+/**
+ * @brief Own implementation of GraphicsScene
+ * It adds mouse/keyboard handling and drag'n'drop support for the editor.
+ **/
+class GraphicsScene : public QGraphicsScene {
+	//TODO actually implement the whole thing
+};
+
+#endif // GRAPHICSSCENE_H
