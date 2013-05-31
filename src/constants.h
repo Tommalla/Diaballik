44d9f83809a12683f9d736af28e208eed4586d85
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-31 21:14:58 +0200

    Added contants.

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+/* Global contants that we cannot move to a settings file */
+
+#include <QString>
+
+const QString AUTHOR = "Tomasz [Tommalla] Zakrzewski, tz336079@students.mimuw.edu.pl";
+const QString VERSION = "v0.1 beta";
+const QString APP_NAME = "Diaballik " + VERSION;
+
+#endif
