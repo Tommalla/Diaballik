af491f2ced3747ffc20102f9e29dc933181181e5
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-31 20:58:32 +0200

    Added some further MainWindow implementation.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,14 +1,23 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include "constants.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), 
+					ui(new Ui::MainWindow) {
+	this->ui->setupUi(this);
+	
+	//appearance settings - title, position, size
+	this->setWindowTitle(APP_NAME);
+	//TODO: take window position and size from settings
+	
+	//creating all necessary objects
+	//TODO - implement and create 'em
+	
+	//connecting signals
+	QObject::connect(ui->actionZakoncz, SIGNAL(triggered()), this, SLOT(close()));
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+	//TODO write necessary settings to settings
+	delete ui;
 }
