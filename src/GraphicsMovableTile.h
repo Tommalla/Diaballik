592d2e5af6740a350ae3f69eb1b3a7ab843b8a2c
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-01 16:35:12 +0200

    Added GraphicsMovableTile (draft).

---
+++ src/GraphicsMovableTile.h
@@ -0,0 +1,24 @@
+/* Tomasz [Tommalla] Zakrzewski, 2013
+All rights reserved */
+
+#ifndef GRAPHICSMOVABLETILE_H
+#define GRAPHICSMOVABLETILE_H
+
+#include "GraphicsTile.h"
+
+
+class GraphicsMovableTile : public GraphicsTile {
+	private:
+		//TODO qpropertyanimation and stuff like that
+	public:
+		/**
+		 * @brief Moves the tile to a new position (instantly)
+		 **/
+		void changePosition(const int newX, const int newY);
+		/**
+		 * @brief Moves the tile to a new position (smoothly and according to the game rules)
+		 **/
+		void move(const int newX, const int newY);
+};
+
+#endif // GRAPHICSMOVABLETILE_H
