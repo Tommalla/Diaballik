2d2b98142de881cf51aca6fa8be9de929fd33a61
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-01 19:28:57 +0200

    Added some graphics and changed directories' names.

--- src/gameConstants.h
+++ src/gameConstants.h
@@ -4,12 +4,13 @@
 /* Global contants that we cannot move to a settings file */
 
 #include <QString>
+#include <QDir>
 
 const QString AUTHOR = "Tomasz [Tommalla] Zakrzewski, tz336079@students.mimuw.edu.pl";
 const QString VERSION = "v0.1 beta";
 const QString APP_NAME = "Diaballik " + VERSION;
 
-const QString APP_LOCATION = "~/.diaballik/";
+const QString APP_LOCATION = QDir::homePath() + "/.diaballik/";
 const QString SETTINGS_FILE = APP_LOCATION + "config.ini";
 
 #endif
