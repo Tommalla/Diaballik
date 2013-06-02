6cf66d5063b8b3524ed822433398ef1829018d25
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-02 11:24:14 +0200

    Further NewGameDialog implementation.

--- src/NewGameDialog.cpp
+++ src/NewGameDialog.cpp
@@ -1,14 +1,73 @@
 #include "NewGameDialog.h"
 #include "ui_NewGameDialog.h"
 
-NewGameDialog::NewGameDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::NewGameDialog)
-{
-    ui->setupUi(this);
+//FIXME this is not the best implementation possible... (lots of delicious pasta)
+
+void NewGameDialog::setWidgetUsable (QWidget* widget, const bool state) {
+	widget->setEnabled(state);
+	widget->setVisible(state);
 }
 
-NewGameDialog::~NewGameDialog()
-{
-    delete ui;
+
+NewGameDialog::NewGameDialog(QWidget *parent) : QDialog(parent), ui(new Ui::NewGameDialog) {
+	this->ui->setupUi(this);
+	
+	//TODO make it shrink...
+	//this->ui->groupBox->setSizePolicy(Qt::MinimumSize);
+	//this->ui->groupBox_2->setSizePolicy(Qt::MinimumSize);
+	
+	this->setWidgetUsable(this->ui->blackListWidget, false);
+	this->setWidgetUsable(this->ui->whiteListWidget, false);
+	
+	//create entries in comboBoxes
+	QComboBox* tempComboBoxTab[2] = { this->ui->blackComboBox, this->ui->whiteComboBox };
+	
+	for (QComboBox* comboBox: tempComboBoxTab) {
+		comboBox->addItem("Human", HUMAN_PLAYER);
+		comboBox->addItem("AI Player", AI_PLAYER);
+	}
+
+	//fill bots list
+	QListWidget* tempListWidgetTab[2] = { this->ui->whiteListWidget, this->ui->blackListWidget };
+	
+	for (QListWidget* listWidget: tempListWidgetTab) {
+		listWidget->addItem("Here be bots! (in the future)");
+	}
+	
+	QObject::connect(this->ui->blackComboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(changeBlackGroupBox()));
+	QObject::connect(this->ui->whiteComboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(changeWhiteGroupBox()));
+}
+
+const ApplicationPlayerType NewGameDialog::getBlackPlayerType() const {
+	return (ApplicationPlayerType)this->ui->blackComboBox->itemData(this->ui->blackComboBox->currentIndex()).toInt();
+}
+
+const ApplicationPlayerType NewGameDialog::getWhitePlayerType() const {
+	return (ApplicationPlayerType)this->ui->whiteComboBox->itemData(this->ui->whiteComboBox->currentIndex()).toInt();
+}
+
+NewGameDialog::~NewGameDialog() {
+	delete ui;
+}
+
+void NewGameDialog::changeWhiteGroupBox () {
+	switch (this->getWhitePlayerType()) {
+		case HUMAN_PLAYER:
+			this->setWidgetUsable(this->ui->whiteListWidget, false);
+			break;
+		case AI_PLAYER:
+			this->setWidgetUsable(this->ui->whiteListWidget, true);
+			break;
+	}
+}
+
+void NewGameDialog::changeBlackGroupBox () {
+	switch (this->getBlackPlayerType()) {
+		case HUMAN_PLAYER:
+			this->setWidgetUsable(this->ui->blackListWidget, false);
+			break;
+		case AI_PLAYER:
+			this->setWidgetUsable(this->ui->blackListWidget, true);
+			break;
+	}
 }
--- src/NewGameDialog.h
+++ src/NewGameDialog.h
@@ -2,21 +2,32 @@
 #define NEWGAMEDIALOG_H
 
 #include <QDialog>
+#include "gameEnums.h"
 
 namespace Ui {
-class NewGameDialog;
+	class NewGameDialog;
 }
 
-class NewGameDialog : public QDialog
-{
-    Q_OBJECT
+/**
+ * @brief A graphical New Game Dialog. Allows to choose players and set some
+ * configuration for them.
+ **/
+class NewGameDialog : public QDialog {
+	Q_OBJECT;
+	
+	private:
+		Ui::NewGameDialog *ui;
+		void setWidgetUsable(QWidget* widget, const bool state = true);
     
-public:
-    explicit NewGameDialog(QWidget *parent = 0);
-    ~NewGameDialog();
-    
-private:
-    Ui::NewGameDialog *ui;
+	public:
+		explicit NewGameDialog(QWidget *parent = 0);
+		const ApplicationPlayerType getWhitePlayerType() const;
+		const ApplicationPlayerType getBlackPlayerType() const;
+		~NewGameDialog();
+		
+	private slots:
+		void changeWhiteGroupBox();
+		void changeBlackGroupBox();
 };
 
 #endif // NEWGAMEDIALOG_H
