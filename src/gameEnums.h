3d2715e629c20d70423a597274e339ff9f59d275
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-02 10:41:37 +0200

    Added gameEnums.h .

---
+++ src/gameEnums.h
@@ -0,0 +1,13 @@
+#ifndef GAME_ENUMS_H
+#define GAME_ENUMS_H
+
+/**
+ * @brief Defines possible player types (classes)
+ **/
+enum ApplicationPlayerType {
+	HUMAN_PLAYER,
+	AI_PLAYER,
+	NETWORK_PLAYER	//future idea
+};
+
+#endif
